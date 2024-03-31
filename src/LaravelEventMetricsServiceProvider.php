73940802ff98f957246af62d60bcdbdc57ce7234
Ben Colegate <BennyC@users.noreply.github.com>
2024-03-31 21:19:14 +0100

    Clean up

--- src/LaravelEventMetricsServiceProvider.php
+++ src/LaravelEventMetricsServiceProvider.php
@@ -10,16 +10,8 @@ class LaravelEventMetricsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-event-metrics')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-event-metrics_table')
-            ->hasCommand(LaravelEventMetricsCommand::class);
+            ->hasConfigFile('config');
     }
 }
